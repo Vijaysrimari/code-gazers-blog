f1c2507bc8fb37a31d3e75fa9ea7ed69d383acab
SujithaRajan <sujitha.p2024aids@sece.ac.in>
2024-12-27 10:48:57 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function addComment(event) {
     const commentInput = event.target.previousElementSibling;
     const commentSection = commentInput.previousElementSibling;
 
-    if (commentInput.value.trim() !== ) {
+    if (commentInput.value.trim() !=="" ) {
         const newComment = document.createElement('p');
         newComment.textContent = commentInput.value;
         commentSection.appendChild(newComment);
