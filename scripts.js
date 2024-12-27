e922fe50d90af572ea2ea28cded4cee92850f9da
SujithaRajan <sujitha.p2024aids@sece.ac.in>
2024-12-27 11:06:15 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ function addComment(event) {
     const commentInput = event.target.previousElementSibling;
     const commentSection = commentInput.previousElementSibling;
 
-    if (commentInput.value.trim() !== "") {
+    if (commentInput.value.trim() !=="" ) {
         const newComment = document.createElement('p');
-        newComment.textContent = commentInput.value;
+        new Comment.textContent = commentInput.value;
         commentSection.appendChild(newComment);
         commentInput.value = '';
     } else {
-        alert("Comment cannot be empty!");
+        alert("Comment can be empty!");
     }
 }
